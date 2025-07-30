31d17d356855d599248dc48ea8a3b2bc89b97d5a
nileshMaurya123 <nilesh7874@gmail.com>
2025-07-30 17:39:55 +0530

    changes done

---
+++ strings2.js
@@ -0,0 +1,7 @@
+let myname = "My name is Anurag";
+console.log(myname.length);
+console.log(myname.toUpperCase());
+console.log(myname.toLowerCase());
+
+let anothermessage = "        Hi I am Nilesh";
+console.log(anothermessage.trim());
