09917b38e5bf05b2cfb1ff929b060aa407164af6
nileshMaurya123 <nilesh7874@gmail.com>
2025-07-30 17:18:34 +0530

    changes done

---
+++ strings.js
@@ -0,0 +1,9 @@
+let firstname = "Anurag";
+let lastname = "Singh";
+let age = 25;
+let isHappy = true;
+
+let userIntro = " Hi my name is " + firstname + " " + lastname;
+console.log(userIntro);
+
+let prompt = "Please enter your name";
